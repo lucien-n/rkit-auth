7d789016411268909c6ce5703c40af803e9c5dd2
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2024-01-12 16:58:18 +0100

    fix: check passwords on authorize and auth errors

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,16 +1,25 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AuthError, type Credentials } from '$lib/types';
 import type { User } from '@auth/sveltekit';
+import type { RequestEvent } from '@sveltejs/kit';
+import bcrypt from 'bcrypt';
 import { handleRemult } from '../../hooks/handleRemult';
 import { UsersController } from '../../shared/users/users.controller';
 
-export const authorize = async (credentials: any, request: Request): Promise<User | null> => {
-	const user = await handleRemult.withRemult({ request } as any, () =>
+export const authorize = async (
+	credentials: Credentials,
+	request: RequestEvent<Partial<Record<string, string>>, string | null>
+): Promise<User | null> => {
+	const user = await handleRemult.withRemult(request, () =>
 		UsersController.findByEmail(credentials.email, { credentials: true })
 	);
 
-	if (!user) return null;
+	if (!user || !user.credentials) throw AuthError.UserNotFound;
 
 	const { id, username } = user;
+
+	const passwordsMatch = await bcrypt.compare(credentials.password, user.credentials.passwordHash);
+
+	if (!passwordsMatch) throw AuthError.InvalidCredentials;
 
 	return {
 		id,
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,8 @@ export type Credentials = {
 	email: string;
 	password: string;
 };
+
+export enum AuthError {
+	InvalidCredentials = 'auth/invalid-credentials',
+	UserNotFound = 'auth/user-not-found'
+}
