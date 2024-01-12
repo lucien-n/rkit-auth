8564f43e39664aed0b5b326e6159bcbb544b2460
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2024-01-12 15:42:17 +0100

    ref

--- src/hooks/handleRemult.ts
+++ src/hooks/handleRemult.ts
@@ -1,7 +1,5 @@
 import { PRIVATE_DATABASE_URL } from '$env/static/private';
-import { Task } from '$remult/tasks/task.entity';
-import { UserCredentials } from '$remult/user-credentials/user-credentials.entity';
-import { User } from '$remult/users/user.entity';
+import { controllers, entities } from '$remult';
 import type { UserInfo } from 'remult';
 import { createPostgresDataProvider } from 'remult/postgres';
 import { remultSveltekit } from 'remult/remult-sveltekit';
@@ -9,5 +7,6 @@ import { remultSveltekit } from 'remult/remult-sveltekit';
 export const handleRemult = remultSveltekit({
 	dataProvider: createPostgresDataProvider({ connectionString: PRIVATE_DATABASE_URL }),
 	getUser: async (event) => (await event.locals?.getSession())?.user as UserInfo,
-	entities: [Task, User, UserCredentials]
+	entities,
+	controllers
 });
