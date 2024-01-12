b0a01d23ed6ae0a5edb7421d79dffbb146c3a4d7
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2024-01-12 16:51:35 +0100

    export tasks conroller

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,6 +1,7 @@
 import { Task } from './tasks/task.entity';
 import { UsersController } from './users/users.controller';
 import { User } from './users/user.entity';
+import { TasksController } from './tasks/tasks.controller';
 
 export const entities = [User, Task];
-export const controllers = [UsersController];
+export const controllers = [UsersController, TasksController];
