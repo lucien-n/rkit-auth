0f7290e0e78226cfeaaa7af25a83d326c6e9d0ca
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-13 11:39:01 +0100

    use userscontroller in handleAuth

--- src/hooks/handleAuth.ts
+++ src/hooks/handleAuth.ts
@@ -1,24 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PRIVATE_AUTH_SECRET } from '$env/static/private';
 import { authorize } from '$lib/server/auth';
-import { User } from '$remult/users/user.entity';
+import type { Credentials as TCredentials } from '$lib/types';
+import { UsersController } from '$remult/users/users.controller';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Credentials from '@auth/sveltekit/providers/credentials';
 import type { Handle } from '@sveltejs/kit';
-import { remult } from 'remult';
 
 export const handleAuth = SvelteKitAuth(async () => {
 	const authOptions = {
 		providers: [
 			Credentials({
 				type: 'credentials',
-				authorize: async (credentials, request) => await authorize(credentials, request)
+				authorize: async (credentials, request) =>
+					await authorize(credentials as TCredentials, request)
 			})
 		],
 		callbacks: {
 			session: async ({ session }: { session: any }) => {
 				if (session.user?.id) {
-					const user = await remult.repo(User).findFirst({ id: session.user.id });
+					const user = await UsersController.findById(session.user.id);
 
 					return {
 						...session,
