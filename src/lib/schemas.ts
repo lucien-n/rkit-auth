db045027df9a06c48e11b27f11a3aa9d3fa6004f
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-13 11:41:43 +0100

    feat: sign in schema uses creaste-user input

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,23 +1,8 @@
+import { createUserSchema } from '$remult/users/dto/create-user.input';
 import { z } from 'zod';
 
-const email = z.string().email();
-const password = z.string().min(8).max(255);
-const username = z.string().min(2).max(24);
-
-export const signInSchema = z.object({
-	email,
-	password
-});
-
+export const signInSchema = createUserSchema.omit({ username: true });
 export type SignInSchema = typeof signInSchema;
-
-export const signUpSchema = z.object({
-	username,
-	email,
-	password
-});
-
-export type SignUpSchema = typeof signUpSchema;
 
 export const taskSchema = z.object({
 	title: z.string().min(3).max(250)
