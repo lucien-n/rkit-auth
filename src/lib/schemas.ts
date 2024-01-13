2dfedfd21615beb3c2628728a12117f628bbc8ff
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-13 18:52:18 +0100

    feat: removed task schema from front

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,11 +1,4 @@
 import { createUserSchema } from '$remult/users/dto/create-user.input';
-import { z } from 'zod';
 
 export const signInSchema = createUserSchema.omit({ username: true });
 export type SignInSchema = typeof signInSchema;
-
-export const taskSchema = z.object({
-	title: z.string().min(3).max(250)
-});
-
-export type TaskSchema = typeof taskSchema;
