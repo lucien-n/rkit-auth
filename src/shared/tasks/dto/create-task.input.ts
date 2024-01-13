ce58334161e9e7b15f0e9878012e594d93a152be
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-13 11:44:24 +0100

    feat: back task create input as zod object

--- src/shared/tasks/dto/create-task.input.ts
+++ src/shared/tasks/dto/create-task.input.ts
@@ -1,3 +1,16 @@
-import type { Task } from '../task.entity';
+import { getZLengthError as gzle, getZStringErrors as gzse } from '$remult/helpers';
+import { z } from 'zod';
 
-export type CreateTaskInput = Pick<Task, 'title'>;
+const TITLE_MIN = 2;
+const TITLE_MAX = 250;
+
+const createTaskSchema = z.object({
+	title: z
+		.string(gzse('title'))
+		.min(TITLE_MIN, gzle(TITLE_MIN, 'title'))
+		.max(TITLE_MAX, gzle(TITLE_MAX, 'title', 'max'))
+});
+
+export type CreateTaskSchema = typeof createTaskSchema;
+
+export type CreateTaskInput = z.infer<CreateTaskSchema>;
