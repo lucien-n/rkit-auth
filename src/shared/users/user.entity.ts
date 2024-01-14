7bb499994f8e029aa3264019e7f0188b983d8ad6
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-14 15:51:27 +0100

    feat: removed field validate since we check with zod in backend methods

--- src/shared/users/user.entity.ts
+++ src/shared/users/user.entity.ts
@@ -1,19 +1,13 @@
-import { validateWithRules } from '$remult/helpers';
 import { Role } from '$remult/roles';
 import { UserCredentials } from '$remult/user-credentials/user-credentials.entity';
 import { Entity, Fields, Relations } from 'remult';
-import userRules from './user.rules';
 
 @Entity<User>('users')
 export class User {
 	@Fields.cuid()
 	id!: string;
 
-	@Fields.string({
-		validate: ({ username }: { username: string }) => {
-			validateWithRules(username, userRules.username);
-		}
-	})
+	@Fields.string()
 	username!: string;
 
 	@Fields.object()
