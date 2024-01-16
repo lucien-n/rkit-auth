c2b569f7ab781dcf78d96c3946ad762a8e028e8e
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-16 14:18:48 +0100

    display nothing if not user

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,36 +28,39 @@
 	});
 </script>
 
-<div class="font-mono">
-	<div class="my-12 text-center">
-		<h1 class="text-5xl tracking-tight">
-			Welcome, <strong>{user?.name}</strong>
-		</h1>
-		<div class="flex justify-center gap-2">
-			<p class="text-muted-foreground">{user?.id}</p>
-			-
-			<p class="text-muted-foreground">
-				{#each user.roles as role}
-					{role}
-				{/each}
-			</p>
+{#if user}
+	<div class="font-mono">
+		<div class="my-12 text-center">
+			<h1 class="text-5xl tracking-tight">
+				Welcome, <strong>{user?.name}</strong>
+			</h1>
+			<div class="flex justify-center gap-2">
+				<p class="text-muted-foreground">{user?.id}</p>
+				-
+
+				<p class="text-muted-foreground">
+					{#each user.roles as role}
+						{role}
+					{/each}
+				</p>
+			</div>
 		</div>
+		<main class="container">
+			<TaskForm form={data.form} />
+
+			<Separator orientation="horizontal" class="my-6" />
+
+			<div class="flex flex-col gap-3">
+				{#if tasks?.length}
+					{#each tasks as task (task.id)}
+						<Task {task} />
+					{/each}
+				{:else}
+					<div class="items-center text-center text-3xl italic text-muted-foreground">
+						You don't have any task!
+					</div>
+				{/if}
+			</div>
+		</main>
 	</div>
-	<main class="container">
-		<TaskForm form={data.form} />
-
-		<Separator orientation="horizontal" class="my-6" />
-
-		<div class="flex flex-col gap-3">
-			{#if tasks?.length}
-				{#each tasks as task (task.id)}
-					<Task {task} />
-				{/each}
-			{:else}
-				<div class="items-center text-center text-3xl italic text-muted-foreground">
-					You don't have any task!
-				</div>
-			{/if}
-		</div>
-	</main>
-</div>
+{/if}
