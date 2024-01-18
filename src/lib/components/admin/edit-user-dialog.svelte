80d6ecf0eaaec34c36d018c33ce8a354b1571a53
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-18 18:40:49 +0100

    display error

--- src/lib/components/admin/edit-user-dialog.svelte
+++ src/lib/components/admin/edit-user-dialog.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { User } from '$remult/users/user.entity';
 	import { UsersController } from '$remult/users/users.controller';
+	import { Alert } from '$shadcn/components/ui/alert';
 	import { Button } from '$shadcn/components/ui/button';
 	import * as Dialog from '$shadcn/components/ui/dialog';
 	import { Input } from '$shadcn/components/ui/input';
@@ -9,14 +10,22 @@
 	export let user: User;
 	export let open = false;
 
+	let message = '';
+
 	export const save = async () => {
 		const {
 			id,
 			username,
 			credentials: { email }
 		} = user;
-		await UsersController.update({ id, username, email });
-		open = false;
+
+		try {
+			await UsersController.update({ id, username, email });
+			message = '';
+			open = false;
+		} catch (e) {
+			message = JSON.parse(e.message)?.[0]?.message ?? 'An error occured';
+		}
 	};
 </script>
 
@@ -25,6 +34,11 @@
 		<Dialog.Header>
 			<Dialog.Title>Edit user</Dialog.Title>
 		</Dialog.Header>
+		{#if message}
+			<Alert variant="destructive">
+				{message}
+			</Alert>
+		{/if}
 		<div class="grid gap-4 py-4">
 			<div class="grid grid-cols-4 items-center gap-4">
 				<Label for="username" class="text-right">Username</Label>
